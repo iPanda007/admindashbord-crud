51ff41c641bee076060b8b0366caf98ec7ea161c
iPanda <aungkyawkhaing2000@gmail.com>
2022-12-24 22:24:39 +0630

    update

--- src/locals/useLocals.ts
+++ src/locals/useLocals.ts
@@ -3,9 +3,11 @@ import { allLangs, defaultLang } from './config';
 
 export default function useLocales() {
     const { i18n, t: translate } = useTranslation();
-    
+    const langStorage = typeof window !== "undefined" ? localStorage.getItem("i18nextLng") : "";
+
+    const currentLang = allLangs.find((_lang) => _lang.value === langStorage) || defaultLang;
+
+    const handleChangeLanguage = (newLang:string) => {
+        
+    }
 }
-
-const langStorage = typeof window !== "undefined" ? localStorage.getItem("i18nextLng") : "";
-
-const currentLang = allLangs.find
