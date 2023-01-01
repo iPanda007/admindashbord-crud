4afef5fd771f0aa93f055970d1367627af2f5e95
iPanda007 <aungkyawkhaing2000@gmail.com>
2023-01-01 19:36:00 +0630

    update

--- src/pages/admin/Education/index.tsx
+++ src/pages/admin/Education/index.tsx
@@ -1,8 +1,100 @@
-import React from "react";
-import search from '../../../assets/resources/search.png'
+import React, { useState } from "react";
+import search from "../../../assets/resources/search.png";
+import allergy from "../../../assets/resources/allergy.png";
+import More from "../../../assets/resources/more.png";
+import pickEater from "../../../assets/resources/pick.png";
+import { createRipples } from 'react-ripples'
+
+const MyRipples = createRipples({
+  color: '#ffffff15',
+  during: 2200,
+})
+
+
+type IDummpyData = {
+  id: string;
+  petName: string;
+  status: string;
+  pawrent: string;
+  bread: string;
+  gender: string;
+  dateOfBirth: string;
+  contact: string;
+  address: string;
+};
+
+const dummpyData = [
+  {
+    id: "B-0025",
+    petName: "Milo",
+    status: allergy,
+    pawrent: "The' Nu San",
+    bread: "Beogle",
+    gender: "Male",
+    dateOfBirth: "1.5.2021",
+    contact: "09966928899",
+    address: "တိုက်(၅) - အခန်း(001) - လိူင်သိရိအိမ်ရာ",
+  },
+  {
+    id: "S-0189",
+    petName: "Milo",
+    status: allergy,
+    pawrent: "Nay Chi Lin",
+    bread: "Beogle",
+    gender: "Male",
+    dateOfBirth: "1.5.2021",
+    contact: "09966928899",
+    address: "တိုက်(၅) - အခန်း(001) - လိူင်သိရိအိမ်ရာ",
+  },
+  {
+    id: "G-0089",
+    petName: "Milo",
+    status: pickEater,
+    pawrent: "Pink Pink",
+    bread: "Beogle",
+    gender: "Male",
+    dateOfBirth: "1.5.2021",
+    contact: "09966928899",
+    address: "တိုက်(၅) - အခန်း(001) - လိူင်သိရိအိမ်ရာ",
+  },
+  {
+    id: "G-0090",
+    petName: "Milo",
+    status: pickEater,
+    pawrent: "Kyaw Myo Oo",
+    bread: "Beogle",
+    gender: "Male",
+    dateOfBirth: "1.5.2021",
+    contact: "09966928899",
+    address: "တိုက်(၅) - အခန်း(001) - လိူင်သိရိအိမ်ရာ",
+  },
+  {
+    id: "G-0091",
+    petName: "Milo",
+    status: pickEater,
+    pawrent: "Pink Pink",
+    bread: "Beogle",
+    gender: "Male",
+    dateOfBirth: "1.5.2021",
+    contact: "09966928899",
+    address: "တိုက်(၅) - အခန်း(001) - လိူင်သိရိအိမ်ရာ",
+  },
+  {
+    id: "G-0092",
+    petName: "Milo",
+    status: pickEater,
+    pawrent: "Pink Pink",
+    bread: "Beogle",
+    gender: "Male",
+    dateOfBirth: "1.5.2021",
+    contact: "09966928899",
+    address: "တိုက်(၅) - အခန်း(001) - လိူင်သိရိအိမ်ရာ",
+  },
+];
 
 //54bab9
 const Education = () => {
+  const [data, setData] = useState<IDummpyData[]>(dummpyData);
   return (
     <div className=" font-[600] mt-[2rem]">
       <header></header>
@@ -21,10 +113,12 @@ const Education = () => {
               <img src={search} alt="" />
             </div>
           </div>
-          <div className="">
-            <button className="px-3 py-1 bg-[#54bab9] text-white flex space-x-2 font-[400] text-[12px] rounded-full">
-              <span className="mr-1 text-[]">+</span> Add new patient
-            </button>
+          <div className="overflow-hidden">
+            <MyRipples >
+              <button className="px-3 py-1 bg-[#54bab9] text-white  flex space-x-2 font-[400] text-[12px] rounded-full">
+                <span className="mr-1 text-[]">+</span> Add new patient
+              </button>
+            </MyRipples>
           </div>
         </div>
         {/* dropDrown */}
@@ -71,9 +165,9 @@ const Education = () => {
 
         <div className=" border-t mt-4 w-full">
           <table className="w-full">
-            <thead className="text-[#54bab9] text-[13px] text-center font-[500] border-b">
+            <thead className="text-[#54bab9] text-[13px] text-left font-[500] border-b">
               <tr className="">
-                <th className="px-4 py-2">
+                <th className="px-4 py-2 text-center">
                   <input type="checkBox" />
                 </th>
                 <th className="px-3 py-2">ID</th>
@@ -88,22 +182,28 @@ const Education = () => {
                 <th></th>
               </tr>
             </thead>
-            <tbody className="text-center text-[13px] font-[400]">
-              {[1, 2, 3, 4, 5].map((item: any, index: number) => (
-                <tr key={index} className="text-center border-b h-[36px] ">
-                  <td className="">
+            <tbody className=" text-[13px] font-[400]">
+              {data.map((item: any, index: number) => (
+                <tr
+                  key={index}
+                  className=" border-b h-[36px] align-middle relative  "
+                >
+                  <td className="text-center">
                     <input type="checkbox" />
                   </td>
-                  <td>B-0025</td>
-                  <td>Milo</td>
-                  <td>icon</td>
-                  <td>THe Nu San</td>
-                  <td>Beogle</td>
-                  <td>Male</td>
-                  <td>1.5.2021</td>
-                  <td>09966928899</td>
+                  <td className="text-center">{item.id}</td>
+                  <td className="text-center">{item.petName}</td>
+                  <td className="text-center ">
+                    <img src={item.status} />
+                  </td>
+                  <td>{item.pawrent}</td>
+                  <td>{item.bread}</td>
+                  <td>{item.gender}</td>
+                  <td>{item.dateOfBirth}</td>
+                  <td>{item.contact}</td>
+                  <td>{item.address}</td>
                   <td>
-                    တိုက်{`(၅)`} - အခန်း{`(001)`} - လိူင်သိရိအိမ်ရာ
+                    <img src={More} width={15} />
                   </td>
                 </tr>
               ))}
