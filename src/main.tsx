8aa8ad905e9a570bd799dc4c0836ead1a9061bcb
iPanda007 <aungkyawkhaing2000@gmail.com>
2023-01-01 20:51:52 +0630

    u

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React,{Suspense} from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Suspense fallback={"loading"}>
+      <Provider store={store}>
         <App />
-        </Suspense>
+      </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/store/index.tsx
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {
+    
+    }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
+
+const { dispatch } = store;
+
+export { dispatch };
